d905d2dceb0a4c3f514ae9aec304f30908bf20ff
tigerding <zhiyuanding01@gmail.com>
2025-01-25 14:51:52 -0500

    feat: adapt radio group buttons to dark mode

--- static/js/redux/ui/RadioGroup.tsx
+++ static/js/redux/ui/RadioGroup.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { selectTheme } from "../state/slices/themeSlice";
+import { useAppSelector } from "../hooks";
 
 interface RadioGroupProps {
   buttons: string[];
@@ -6,33 +8,40 @@ interface RadioGroupProps {
   onChange: (button: string) => void;
 }
 
-const RadioGroup: React.FC<RadioGroupProps> = ({ buttons, active, onChange }) => (
-  <div
-    style={{
-      width: "100%",
-      border: "1px solid #ddd",
-      borderRadius: 5,
-      display: "flex",
-      flex: 1,
-      overflow: "hidden",
-    }}
-  >
-    {buttons.map((button) => (
-      <div
-        onClick={() => onChange(button)}
-        style={{
-          fontSize: 13,
-          textAlign: "center",
-          cursor: "pointer",
-          background: active === button ? "#ddd" : "#f3f3f3",
-          transition: "background 0.3s ease",
-          width: "33.33%",
-        }}
-      >
-        {button[0].toUpperCase() + button.slice(1)}
-      </div>
-    ))}
-  </div>
-);
+const RadioGroup: React.FC<RadioGroupProps> = ({ buttons, active, onChange }) => {
+  const curTheme = useAppSelector(selectTheme);
+
+  const bgColor = curTheme.name === "light" ? "#f3f3f3" : "#2d2e32";
+  const activeColor = curTheme.name === "light" ? "#ddd" : "#5a5d63";
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        border: `1px solid ${activeColor}`,
+        borderRadius: 5,
+        display: "flex",
+        flex: 1,
+        overflow: "hidden",
+      }}
+    >
+      {buttons.map((button) => (
+        <div
+          onClick={() => onChange(button)}
+          style={{
+            fontSize: 13,
+            textAlign: "center",
+            cursor: "pointer",
+            background: active === button ? activeColor : bgColor,
+            transition: "background 0.3s ease",
+            width: "33.33%",
+          }}
+        >
+          {button[0].toUpperCase() + button.slice(1)}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default RadioGroup;
