d445c158cf43fda94bd183cfe281c3a66e77c222
tigerding <zhiyuanding01@gmail.com>
2025-01-31 11:29:17 -0500

    ref: use scss for styling

--- static/js/redux/ui/RadioGroup.tsx
+++ static/js/redux/ui/RadioGroup.tsx
@@ -8,36 +8,25 @@ interface RadioGroupProps {
   onChange: (button: string) => void;
 }
 
+const capitalize = (s: string) => s[0].toUpperCase() + s.slice(1);
+
 const RadioGroup: React.FC<RadioGroupProps> = ({ buttons, active, onChange }) => {
   const curTheme = useAppSelector(selectTheme);
 
-  const bgColor = curTheme.name === "light" ? "#f3f3f3" : "#2d2e32";
-  const activeColor = curTheme.name === "light" ? "#ddd" : "#5a5d63";
-
   return (
-    <div
-      style={{
-        width: "100%",
-        border: `1px solid ${activeColor}`,
-        borderRadius: 5,
-        display: "flex",
-        flex: 1,
-        overflow: "hidden",
-      }}
-    >
+    <div className={["prerequisites-radio-group", curTheme.name].join(" ")}>
       {buttons.map((button) => (
         <div
           onClick={() => onChange(button)}
-          style={{
-            fontSize: 13,
-            textAlign: "center",
-            cursor: "pointer",
-            background: active === button ? activeColor : bgColor,
-            transition: "background 0.3s ease",
-            width: "33.33%",
-          }}
+          className={[
+            "prerequisites-radio-btn",
+            active === button && "active",
+            curTheme.name,
+          ]
+            .filter(Boolean)
+            .join(" ")}
         >
-          {button[0].toUpperCase() + button.slice(1)}
+          {capitalize(button)}
         </div>
       ))}
     </div>
